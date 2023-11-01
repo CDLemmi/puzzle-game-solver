eb54f2be866c384a618902cd1bf9efce1c3565fb
CDLemmi <112967979+CDLemmi@users.noreply.github.com>
2023-10-31 19:02:00 -0700

    added toString() in GameState default constructor to

--- src/java/Tester.java
+++ src/java/Tester.java
@@ -1,9 +1,13 @@
 package src.java;
 
+import src.java.data.GameState;
+
 public class Tester {
 
     public static void main(String[] args) {
         System.out.println("Hello Mom!");
+        GameState state = new GameState();
+        System.out.println(state);
     }
 
 }
--- src/java/data/GameState.java
+++ src/java/data/GameState.java
@@ -10,6 +10,36 @@ public class GameState {
     private int[] b;
 
 
+    public GameState() {
+        setW(5);
+        setV(7);
+        setH(new int[] {0,2,15,17});
+        setB(new int[] {4,8,13,19});
+    }
+
+
+
+    public String toString() {
+        StringBuilder s = new StringBuilder("GameState(W=");
+        s.append(getW());
+        s.append("|V=");
+        s.append(getV());
+        s.append("|H=");
+        for(int i = 0; i < 4; i++) {
+            s.append(getH()[i]);
+            s.append(",");
+        }
+        s.setCharAt(s.length() - 1, '|');
+        s.append("B=");
+        for(int i = 0; i < 4; i++) {
+            s.append(getB()[i]);
+            s.append(",");
+        }
+        s.setCharAt(s.length() - 1, ')');
+        return s.toString();
+    }
+
+
 
     void setW(int n) {
         w = n;
